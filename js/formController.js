48214eb279fc00b1ca6350d8cc6177913559d863
Sebastian Berge <sebastian@berge.im>
2019-10-04 14:29:09 +0200

    Implementing JS
    Using JavaScript for the switching of forms on the main page.

---
+++ js/formController.js
@@ -0,0 +1,20 @@
+"use strict"
+
+let formButtonOne = document.getElementById("form__one")
+let formButtonTwo = document.getElementById("form__two")
+
+formButtonOne.onclick = function() {
+	console.log("LEFT CLICKED")
+	document.getElementById("form__book").style.display = "block"
+	document.getElementById("form__view").style.displat = "none"
+	formButtonOne.classList.add("active")
+	formButtonTwo.classList.remove("active")
+}
+
+formButtonTwo.onclick = function() {
+	console.log("RIGHT CLICKED")
+	document.getElementById("form__book").style.display = "none"
+	document.getElementById("form__view").style.displat = "block"
+	formButtonOne.classList.remove("active")
+	formButtonTwo.classList.add("active")
+}
