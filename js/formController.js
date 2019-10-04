922e5142586ed06d32336d5fd754423eb417e167
Sebastian Berge <sebastian@berge.im>
2019-10-04 15:09:55 +0200

    :boom:
    Wokring on multiple things

--- js/formController.js
+++ js/formController.js
@@ -6,7 +6,7 @@ let formButtonTwo = document.getElementById("form__two")
 formButtonOne.onclick = function() {
 	console.log("LEFT CLICKED")
 	document.getElementById("form__book").style.display = "block"
-	document.getElementById("form__view").style.displat = "none"
+	document.getElementById("form__view").style.display = "none"
 	formButtonOne.classList.add("active")
 	formButtonTwo.classList.remove("active")
 }
@@ -14,7 +14,7 @@ formButtonOne.onclick = function() {
 formButtonTwo.onclick = function() {
 	console.log("RIGHT CLICKED")
 	document.getElementById("form__book").style.display = "none"
-	document.getElementById("form__view").style.displat = "block"
+	document.getElementById("form__view").style.display = "block"
 	formButtonOne.classList.remove("active")
 	formButtonTwo.classList.add("active")
 }
